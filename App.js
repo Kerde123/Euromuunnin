7b270aa8316beedd3a117b25d9f3cd839cfdda67
Kerde123 <kerttu.vanhala@gmail.com>
2023-02-13 12:08:53 +0200

    Ei toimi ollenkaan

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
+import { API_URL, API_TOKEN } from '@env';
 
 export default function App() {
+ 
+  
+  const [selectedValue, setSelectedValue] = useState('');
+  const objRate = {}
+
+  const getRate = () => {
+    fetch(`${API_URL}/users`, {
+      headers: {
+        'Authorization': `Bearer ${API_TOKEN}`
+      }
+    })
+    .then(response => response.json())
+    .then(responseJson => setRate(responseJson.objRate))
+    .catch(error => { 
+      Alert.alert('Error', error.message); 
+    });    
+  }
+
+  const convertValue {
+
+  }
+
+  const ItemSeparator = () => 
+  <View style={{
+      height: 1,
+      width: "100%",
+      backgroundColor: "grey"
+    }}
+    /> 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <Picker
+        selectedValue={getRate}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+      <Button title = "Convert" onPress = {convertValue} />
     </View>
   );
 }
